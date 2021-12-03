29057362f265bffded62adaa406050107d98793f
SoVa-web <oolga200000001@ukr.net>
2021-12-03 15:29:21 +0200

    saving tasks in storage

--- app/src/main/java/com/example/lab2_suprun/FragmentContentInput.java
+++ app/src/main/java/com/example/lab2_suprun/FragmentContentInput.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
+import android.content.ContextWrapper;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +19,10 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.view.Gravity;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FragmentContentInput extends Fragment{
 
@@ -33,13 +40,12 @@ public class FragmentContentInput extends Fragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View rootView =
-                inflater.inflate(R.layout.fragment_content, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_content, container, false);
 
         RadioGroup difficulty = rootView.findViewById(R.id.difficulty);
         RadioGroup type_task = rootView.findViewById(R.id.type);
         task = rootView.findViewById(R.id.editTextTextMultiLine2);
-        Button button_ok = (Button) rootView.findViewById(R.id.button);
+        Button button_ok = rootView.findViewById(R.id.button);
 
         button_ok.setOnClickListener(this::onClick);
 
@@ -75,19 +81,43 @@ public class FragmentContentInput extends Fragment{
         return rootView;
     }
 
+    public void saveTask(String res){
+
+        try(FileWriter writer = new FileWriter((Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS)).toString() + "/saved_task.txt", true))
+        {
+            writer.write(res);
+            writer.append('\n');
+            writer.flush();
+            Toast empty_field = Toast.makeText(getActivity(),
+                    "Завдання успішно збережено",
+                    Toast.LENGTH_SHORT);
+            empty_field.show();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+            Toast empty_field = Toast.makeText(getActivity(),
+                    "Помилка зберігання",
+                    Toast.LENGTH_SHORT);
+            empty_field.show();
+        }
+    }
+
     @SuppressLint("SetTextI18n")
     public void onClick(View view) {
         if (val_dif == null || val_type == null || TextUtils.isEmpty(task.getText())) {
             Toast empty_field = Toast.makeText(getActivity(),
                     "Ви не ввели запитання або не здійснили вибір складності/типу завдання.",
                     Toast.LENGTH_SHORT);
-            empty_field.setGravity(Gravity.CENTER, 0, 0);
             empty_field.show();
         } else {
             result_output = "Завдання:   " + task.getText() + "\nТип завдання: " + val_type + "\nРівень складності:  " + val_dif;
+            this.saveTask(result_output);
             OnSelectedButtonListener listener = (OnSelectedButtonListener) getActivity();
             assert listener != null;
             listener.onButtonSelected(result_output);
+
         }
     }
+
 }
